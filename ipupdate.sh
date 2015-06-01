ec6250ad4683a5efa5278b9f23b516b4f9f37a67
jamesdesmond <jamesdesmond13@gmail.com>
2015-06-01 16:40:47 -0400

    Added initial Files
    TODO: Make a README

---
+++ ipupdate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#source $HOME/.bashrc
+NEWIP=$( curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//') 
+OLDIP=$(cat /home/pi/ip.txt)
+if [ -z "$NEWIP" ]; then
+	exit
+fi
+if [ "$NEWIP" != "$OLDIP" ]; then
+	#echo "true"
+	echo "$NEWIP" > /home/pi/ip.txt
+	mutt -s "IP" 8608332915@vtext.com < /home/pi/ip.txt
+fi
+
+
+
